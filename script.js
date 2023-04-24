9f8a7a1edbdab5713e921ca002a6bdec3d72fc1d
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-24 20:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	let s = "";
+	for(let i = 0;i<str.length;i++) {
+		if(str.charAt(i)!=' ') {
+			s+= str.charAt(i);
+		}
+	}
+	for(let i = 0;i<s.length/2;i++) {
+		if(s.charAt(i)!=s.charAt(s.length-i-1)) {
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
